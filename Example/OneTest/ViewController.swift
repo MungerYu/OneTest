809580050394d08352d3a80aa6e0013e282a023a
MungerYu <linming348@126.com>
2022-08-16 16:51:26 +0800

    version

--- Example/OneTest/ViewController.swift
+++ Example/OneTest/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import OneTest
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        OneTest.testPrint()
     }
 
     override func didReceiveMemoryWarning() {
